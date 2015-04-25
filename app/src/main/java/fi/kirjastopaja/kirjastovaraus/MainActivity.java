29d4e3bf02fdb5d16e542f94098e7c9a9583f77a
Anoop Vijayan <anovil@anovil-mac.local>
2015-04-25 23:26:44 +0300

    initial draft working version of app

---
+++ app/src/main/java/fi/kirjastopaja/kirjastovaraus/MainActivity.java
@@ -0,0 +1,114 @@
+package fi.kirjastopaja.kirjastovaraus;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private static final String BASE_URL = "http://varaus.kirjastopaja.fi/Web/?";
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // web view
+       this.mWebView = (WebView) findViewById(R.id.mainWebView);
+
+        // Enable javascript
+        WebSettings webSettings = this.mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        // dont open separate url
+        this.mWebView.setWebViewClient(new WebViewClient(){
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        // load the page every 30 secs
+            mWebView.loadUrl(BASE_URL);
+        new ReloadWebView(this, 30, mWebView);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // courtesy: http://stackoverflow.com/questions/8209078/how-to-refresh-the-webview-in-android-using-a-timer
+    protected class ReloadWebView extends TimerTask {
+        Activity context;
+        Timer timer;
+        WebView wv;
+
+        public ReloadWebView(Activity context, int seconds, WebView wv) {
+            this.context = context;
+            this.wv = wv;
+
+            timer = new Timer();
+        /* execute the first task after seconds */
+            timer.schedule(this,
+                    seconds * 1000,  // initial delay
+                    seconds * 1000); // subsequent rate
+
+        /* if you want to execute the first task immediatly */
+        /*
+        timer.schedule(this,
+                0,               // initial delay null
+                seconds * 1000); // subsequent rate
+        */
+        }
+
+        @Override
+        public void run() {
+            if (context == null || context.isFinishing()) {
+                // Activity killed
+                this.cancel();
+                return;
+            }
+
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    wv.reload();
+                }
+            });
+        }
+    }
+}
