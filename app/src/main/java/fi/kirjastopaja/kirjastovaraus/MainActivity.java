1e915f1dcabec88e1d4bd7897389824c7f4e18d5
manianakara <currnet>
2015-09-27 06:40:02 +0300

    Full screen support from api15 and up

--- app/src/main/java/fi/kirjastopaja/kirjastovaraus/MainActivity.java
+++ app/src/main/java/fi/kirjastopaja/kirjastovaraus/MainActivity.java
@@ -1,10 +1,14 @@
 package fi.kirjastopaja.kirjastovaraus;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -40,6 +44,18 @@ public class MainActivity extends ActionBarActivity {
                 return true;
             }
         });
+
+        // If the Android version is lower than Jellybean, use this call to hide
+        // the status bar.
+        if (Build.VERSION.SDK_INT < 16) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        } else {
+            View decorView = getWindow().getDecorView();
+            // Hide the status bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+        }
 
         // load the page every 30 secs
             mWebView.loadUrl(HUVITUS_URL);
